37b81f5917e559da7a2322b423a821496565cc6a
loudo111 <dorian.chab@hotmail.fr>
2021-11-03 15:08:38 +0100

    :seedling: Ajout de la page d'accueil visiteur

---
+++ accueil.php
@@ -0,0 +1,72 @@
+<?php
+require_once('interface.php');
+?>
+
+<div class="block">
+    <div class="container">
+        <div class="box">
+            <h2 style="text-align: center;"><span style="font-weight: 700;">BTS SIO -&nbsp;</span><strong>BTS Services Informatiques aux Organisations</strong></h2><br/>
+
+					<h5 class="label">Qu’est-ce que le BTS SIO ?</h5>
+					<div class="et_pb_toggle_content clearfix" style="display: block;"><p>Le BTS SIO est un <strong>diplôme national en informatique de niveau bac+2.</strong>&nbsp;Il peut être préparé gratuitement en deux ans dans quelque 130&nbsp; lycées publics ou privés sous contrat présents sur tout le territoire français. Il est aussi proposé par des établissements privés sous contrat d’association avec l’état ou non.&nbsp;A l’issue de cette formation on peut intervenir en entreprise en tant que <strong>développeur d’applications informatiques ou administrateur de réseaux locaux.</strong></p></div>
+			
+					<br/>
+					<h5 class="label">Comment faire pour intégrer un BTS SIO en lycée ?</h5>
+					<div class="et_pb_toggle_content clearfix" style="display: block;">Avec un bac il faut candidater via le portail <a href="https://www.parcoursup.fr/" target="_blank" rel="noopener noreferrer">Parcourssup</a>. On est retenu sur dossier en fonction des résultats obtenus et des remarques des professeurs en première et terminale. On peut être titulaire d’un bac professionnel (SEN prioritairement), technologique (STMG et STI2D prioritairement) ou d’un bac général. Pour avoir le statut d’apprenti, il faut de plus avoir signé un contrat d’apprentissage avec un employeur.</div>
+
+					<br/>
+					<h5 class="label">Comment préparer l’examen du BTS SIO ?</h5>
+					<div class="et_pb_toggle_content clearfix" style="display: block;"><p>On peut se préparer sur deux ans gratuitement en tant qu’étudiant ou en tant qu’apprenti dans un lycée public. La formation est payante dans les lycées privés sous contrat et dans les écoles privées hors contrat.</p>
+						<p>Il est aussi possible de préparer le BTS SIO à distance avec le <a href="http://www.cned.fr/inscription/8TSIODIX" target="_blank" rel="noopener noreferrer">CNED</a>.</p>
+						<p>L’examen peut enfin être préparé en <a href="https://travail-emploi.gouv.fr/formation-professionnelle" target="_blank" rel="noopener noreferrer">formation continue</a> ou en <a href="http://www.vae.gouv.fr/" target="_blank" rel="noopener noreferrer">validation des acquis de l’expérience</a>.</p></div>
+					
+							<div class="et_pb_toggle et_pb_module et_pb_accordion_item et_pb_accordion_item_6  et_pb_toggle_open">
+				
+				
+					<h5 class="label">Quel est le contenu de la formation ?</h5>
+					<div class="et_pb_toggle_content clearfix"><p>La formation comporte 1600 heures réparties sur deux ans. Elle porte, d’une part, sur les compétences informatiques (<strong>administration système et réseau, développement de logiciel, bases de données et cybersécurité</strong>) pour 60% du volume horaire et, d’autre part, sur le <strong>fonctionnement des entreprises</strong> (culture économique juridique et managériale avec une partie appliquée à l’informatique) et les <strong>enseignements généraux</strong> (culture générale et expression, mathématiques pour l’informatique, anglais, LV2 optionnelle) appliqués à l’informatique pour 40% du volume horaire.</p>
+					<p><strong>A la fin du premier semestre</strong>, les étudiants choisissent de se spécialiser dans le domaine de l’administration des systèmes et des réseaux (option <strong>SISR</strong>) ou dans le développement de logiciels (<strong>SLAM</strong>).</p>
+					
+					<br/>
+					<p><strong>Les domaines de compétences du BTS SIO :</strong></p>
+					<p><a href="./picture/competences_bts_sio.png"><img loading="lazy" src="https://www.btsinfo.fr/wp-content/uploads/2020/01/competences_bts_sio.png" width="853" height="389" alt="" class="wp-image-1842 alignnone size-full" srcset="https://www.btsinfo.fr/wp-content/uploads/2020/01/competences_bts_sio.png 2495w, https://www.btsinfo.fr/wp-content/uploads/2020/01/competences_bts_sio-300x137.png 300w, https://www.btsinfo.fr/wp-content/uploads/2020/01/competences_bts_sio-1024x467.png 1024w, https://www.btsinfo.fr/wp-content/uploads/2020/01/competences_bts_sio-768x350.png 768w, https://www.btsinfo.fr/wp-content/uploads/2020/01/competences_bts_sio-1536x701.png 1536w, https://www.btsinfo.fr/wp-content/uploads/2020/01/competences_bts_sio-2048x934.png 2048w, https://www.btsinfo.fr/wp-content/uploads/2020/01/competences_bts_sio-800x365.png 800w, https://www.btsinfo.fr/wp-content/uploads/2020/01/competences_bts_sio-1080x493.png 1080w" sizes="(max-width: 853px) 100vw, 853px"></a></p>
+					<p><em>SISR : Solutions d’Infrastructure, Systèmes et Réseaux</em><br> <em>SLAM : Solutions Logicielles et Applications Métiers</em></p>
+					
+					<br/>
+					<p><strong></strong></p>
+					<p><strong>La formation est très concrète</strong> et permet d’être opérationnel après 2 ans d’études.</p>
+					<div class="et-box et-shadow">
+						Durant la formation de BTS SIO, chaque étudiant bénéficie de <strong>10 à 12 semaines de stage en entreprise</strong>, par exemple 4 semaines au cours de la première année et 7 au cours de la seconde année. Ces stages sont suivis par un professeur tuteur et un professionnel maître de stage.<p></p>
+					<p>En formation en alternance, chaque apprenti est en entreprise durant une partie de la semaine et en centre de formation pour une autre partie.</p></div></div>
+					<p>&nbsp;</p>
+					<p>Pour les enseignements d’informatique, la formation de BTS SIO est organisée en 3 blocs de compétences répartis sur 4 semestres :</p>
+					<br/>
+					<ul>
+					<li>bloc 1 : support et mise à disposition de services informatiques,</li>
+					<li>bloc 2 : administration des systèmes et des réseaux (option SISR) ou&nbsp;conception et développement d’applications (option SLAM),</li>
+					<li>bloc 3 : <strong>cybersécurité. Le bloc est commun en première année et spécifique à chaque option en seconde année.</strong></li>
+					</ul>
+					<br/>
+					<p><strong>Les ateliers de professionnalisation</strong> permettent de consolider l’ensemble des compétences informatiques au travers de la réalisation de projets. Ces 4 heures d’ateliers par semaine ont également pour objectif de développer une véritable autonomie dans la démarche de professionnalisation.</p>
+					<br/>
+					<p><strong>Les autres enseignements du BTS SIO intègrent les problématiques spécifiques au domaine informatique.</strong> Les mathématiques sont associées à l’algorithmique, l’anglais est tourné vers le monde informatique, les étudiants sont initiés aux spécificités juridiques et économiques de leur domaine dans l’enseignement de culture économique, juridique et managériale appliquée.</p>
+					<div class="et-box et-bio">
+					<br/>Au semestre 2, un étudiant ayant choisi la spécialité SISR suivra toujours le bloc 1 et le bloc 3 communs mais suivra le bloc 2 spécifique à l’option choisie. En deuxième année, le bloc 3 relatif à la cybersécurité devient aussi spécifique à l’option, et le bloc 1 reste commun</span></p>
+					<br/><p>Il participera aussi aux ateliers de professionnalisation pendant lesquels il sera amené à travailler sur des projets professionnels au sein d’une équipe d’étudiants. Ces AP (de 4H par semaine pendant toute la formation) visent à développer l’autonomie et le travail collaboratif.</p>
+					<br/><p>Enfin, il pourra choisir l’enseignement optionnel “Certifications professionnelles“ pour développer de façon autonome des compétences complémentaires.</p></div></div>
+					<p>&nbsp;</p>
+					<p><strong>Blocs et horaires hebdomadaires en formation initiale sous statut scolaire</strong></p>
+					<p><strong>&nbsp;<a href="./picture/horaires_btssio.png"><img loading="lazy" src="./picture/horaires_btssio.png" width="2841" height="853" alt="" class="wp-image-1870 alignnone size-full"></a></strong></p>
+					<p><strong></strong></p>
+							
+					<br/>
+					<h5 class="label">Quelles sont les épreuves de l’examen du BTS SIO ?</h5>
+					<div class="et_pb_toggle_content clearfix" style="display: block;"><p><a href="./picture/epreuves_bts_sio.png"><img loading="lazy" src="./picture/epreuves_bts_sio.png" width="759" height="318" alt="" class="wp-image-1824 alignnone size-full" srcset="https://www.btsinfo.fr/wp-content/uploads/2020/01/epreuves_bts_sio.png 759w, https://www.btsinfo.fr/wp-content/uploads/2020/01/epreuves_bts_sio-300x126.png 300w" sizes="(max-width: 759px) 100vw, 759px"></a></p>
+					<p><em>&nbsp;CCF : contrôle en cours de formation</em></p>
+					<p>&nbsp;<em>Organisation des épreuves pour la voie scolaire dans un établissement public ou privé sous contrat</em></p></div> <!-- .et_pb_toggle_content -->
+					</div>
+
+
+			</div>			
+    </div>
+</div>
